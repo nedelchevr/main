d5e3e63c5c55be5e2d032e1b03dc62e028539116
Rusi <123881776+nedelchevr@users.noreply.github.com>
2023-03-06 18:32:29 +0200

    Update L5 Exercise 7.php

--- L5 Exercise 7.php
+++ L5 Exercise 7.php
@@ -4,6 +4,6 @@ $b=31;
 $c=8;
 $x1 = (-$b + sqrt($b*$b-4*$a*$c))/(2*$a);
 $x2 = (-$b - sqrt($b*$b-4*$a*$c))/(2*$a);
-echo $x1;
-echo "\n$x2"
+echo "x1=".round($x1, 2);
+echo "\nx2=".round($x2, 2);
 ?>
